8ca87730e8a02838c8c80238882cb089a62872cb
MiletaBorisova <miletaborisova5@gmail.com>
2025-11-25 12:19:37 +0200

    upload project

---
+++ MyFirstWebApp2025/MyFirstWebApp2025/Controllers/ArticleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebApp2025.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult ById()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstWebApp2025/MyFirstWebApp2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstWebApp2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
